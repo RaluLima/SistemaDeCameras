100c647b8ecd32ad34f9e0e6cb647c71645ad818
Raluzin <raluzin@hotmail.com>
2026-05-12 22:08:48 -0300

    Add webpack resolve alias for @/ paths for Render compatibility

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs']
-  }
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.join(__dirname, "src"),
+    };
+    return config;
+  },
 }
 module.exports = nextConfig
